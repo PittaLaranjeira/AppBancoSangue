e8b2c30e21d18f52c62b1a7350281505be2e6552
pittalaranjeira <vitoria.pitta01@fatec.sp.gov.br>
2023-02-23 00:01:07 -0300

    Aplicativo que simula um banco de sangue -v1

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,43 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor() { }
 
+  tpSangue = ' '
+  qtde = 0
+  apos = 0
+  aneg = 0
+  bpos = 0
+  bneg = 0
+  abpos = 0
+  abneg = 0
+  opos = 0
+  oneg = 0
+
+  doar(formu: { value: { tpSangue: string; qtde: any; }; }) {
+    const tpSangue = formu.value.tpSangue.toUpperCase();
+    const qtde = formu.value.qtde;
+
+
+    this.qtde += parseFloat(qtde);
+
+    if (tpSangue == "A+") {
+      this.apos += parseFloat(qtde);
+    } else if (tpSangue == "A-") {
+      this.aneg += parseFloat(qtde);
+    } else if (tpSangue == "B+") {
+      this.bpos += parseFloat(qtde);
+    } else if (tpSangue == "B-") {
+      this.bneg += parseFloat(qtde);
+    } else if (tpSangue == "AB+") {
+      this.abpos += parseFloat(qtde);
+    } else if (tpSangue == "AB-") {
+      this.abneg += parseFloat(qtde);
+    } else if (tpSangue == "O+") {
+      this.opos += parseFloat(qtde);
+    } else if (tpSangue == "O-") {
+      this.oneg += parseFloat(qtde);
+    }
+
+  }
 }
